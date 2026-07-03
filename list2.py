ff55178a537dfca5d7c2b87cc6ee64e82cdff954
Ben Chen <idben.chen@gmail.com>
2026-07-03 15:20:30 +0800

    串列切片做刪除, 取代與插入

--- list2.py
+++ list2.py
@@ -6,3 +6,11 @@ print(list1[:3])
 print(list1[3:])
 print(list1[::2])
 print(list1[::-1]) # 返轉
+
+
+# list1[1:3] = [] # 刪除串列指定區間
+# list1[1:3] = ["a", "b"] # 取代
+# list1[1:3] = ["a", "b", "c"] # 取代個數不同時, 會自動調整
+list1[1:1] = ["a", "b", "c"] # 插入
+
+print(list1)
