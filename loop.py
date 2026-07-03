b3a2192fdc5461f90444fc4ccff7cf855828fa28
Ben Chen <idben.chen@gmail.com>
2026-07-03 09:49:23 +0800

    迴圈的終止的另一個使用情境

--- loop.py
+++ loop.py
@@ -6,4 +6,16 @@ for num in nums:
     if num == ans:
         print(f"找到了!目標是 {ans}")
         break
-    
+
+print("-"*30)
+datas = [
+    {"id": 1, "name": "Ben", "cell": "0988777888"},
+    {"id": 2, "name": "May", "cell": "0938222111"},
+    {"id": 3, "name": "John", "cell": "0933456789"}
+]
+
+for data in datas:
+    if data["cell"].startswith("0988"):
+        print(f"姓名是: {data["name"]}")
+        break
+print("-"*30)
