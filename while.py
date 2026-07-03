a29c000bb264ed26014f9be5d71837143ecefbe9
Ben Chen <idben.chen@gmail.com>
2026-07-03 09:18:32 +0800

    while 猜數字

--- while.py
+++ while.py
@@ -5,3 +5,14 @@ count = 0
 while count <= 5:
     print(f"這是第 {count} 次的執行")
     count += 1
+print("-"*30)
+
+
+answer = 8
+guess = 0
+
+while answer != guess:
+    guess = int(input("猜數字 1~10："))
+    if answer != guess:
+        print("猜錯了!! 再試一次")
+print("恭喜過關")
