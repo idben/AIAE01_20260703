45e3d6ea2fc77995a4d614d95d9f667a563f4a62
Ben Chen <idben.chen@gmail.com>
2026-07-03 11:38:16 +0800

    巢狀重複建立

--- list1.py
+++ list1.py
@@ -13,3 +13,21 @@ print(list("建立或轉型"))
 print(list(range(3)))
 print(list((11,22))) # tuple 轉 list 
 print(list({"name": "Ben", "phone": "0223255678"})) # 字典轉 list, 是把 key 取出而已
+
+# 重複建立
+# [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# [
+#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# ]
+
+list2 = [0] * 10
+print(list2)
+list3 = [[0] * 10] * 2 # 巢狀 list 的複製(重複), 會指向同一個記憶體位置
+print(list3)
+list3[0][9] = 9
+print(list3)
+
+list4 = [[0] * 10 for _ in range(2)] # 推導式
+list4[0][9] = 9
+print(list4)
