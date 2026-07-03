5a82102338df2f5a27ddc3b4715fecb5147389e6
Ben Chen <idben.chen@gmail.com>
2026-07-03 11:28:16 +0800

    list() 建立或轉型

--- list1.py
+++ list1.py
@@ -5,3 +5,11 @@ list1 = [
 ]
 print(list1[0][0])
 print(list1[1][1])
+print(list1[1][0])
+
+# list() 建立或轉型
+print(list("abc"))
+print(list("建立或轉型"))
+print(list(range(3)))
+print(list((11,22))) # tuple 轉 list 
+print(list({"name": "Ben", "phone": "0223255678"})) # 字典轉 list, 是把 key 取出而已
